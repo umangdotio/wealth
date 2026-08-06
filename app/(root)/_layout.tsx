01027b764db2940400561211a403ab74a8fb79ed
Umang <umangaggarwal7599@gmail.com>
2026-08-06 14:48:38 +0530

    root updated

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "@clerk/expo";
 import { Redirect, Slot } from "expo-router";
 
-export default function RootLayout() {
+export default function RootGroupLayout() {
   const { isLoaded, isSignedIn } = useAuth();
 
   if (!isLoaded) return null;
